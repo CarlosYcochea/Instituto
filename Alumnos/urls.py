c332eac7ac183d3c6e5aaa9490d70df4bcca55f9
CarlosYcochea <134462386+CarlosYcochea@users.noreply.github.com>
2024-06-20 02:22:44 -0400

    segundo commit

--- Alumnos/urls.py
+++ Alumnos/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import index
+from .views import index , crud, alumnosAdd, alumnos_del, alumnos_finEdit, alumnosUpdate
 
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('index', index, name='index'),
+
+    path('crud', crud, name='crud'),
+    path('alumnosAdd', alumnosAdd, name='alumnosAdd'),
+    path('alumnos_del/<str:pk>', alumnos_del, name='alumnos_del'),
+    path('alumnos_finEdit/<str:pk>', alumnos_finEdit, name='alumnos_finEdit'),
+    path('alumnosUpdate', alumnosUpdate, name='alumnosUpdate')
 ]
